2aef193cf704e12879aa7e62e1f23700f06426e9
xchope <p.tran@student.htw-berlin.de>
2025-11-28 00:30:41 +0100

    backend test

--- src/test/java/htw/webtech/mywatchlist/MywatchlistApplicationTests.java
+++ src/test/java/htw/webtech/mywatchlist/MywatchlistApplicationTests.java
@@ -1,12 +1,14 @@
 package htw.webtech.mywatchlist;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class MywatchlistApplicationTests {
 
     @Test
     void dummyTest() {
+        // Einfacher Test, der immer true ist
         assertTrue(true);
     }
 }
