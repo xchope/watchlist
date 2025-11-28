4a33e2fbd7132e2ea482eaa432a083142161bb9d
xchope <p.tran@student.htw-berlin.de>
2025-11-28 01:12:30 +0100

    backend test

--- src/main/java/htw/webtech/mywatchlist/MywatchlistApplication.java
+++ src/main/java/htw/webtech/mywatchlist/MywatchlistApplication.java
@@ -2,17 +2,15 @@ package htw.webtech.mywatchlist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(
-		exclude = {
-				DataSourceAutoConfiguration.class,
-				HibernateJpaAutoConfiguration.class
-		}
-)
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "htw.webtech.mywatchlist.watchlist")
+@EntityScan(basePackages = "htw.webtech.mywatchlist.watchlist")
 public class MywatchlistApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(MywatchlistApplication.class, args);
-	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(MywatchlistApplication.class, args);
+    }
 }
