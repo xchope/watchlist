e508ae4d68760295f0a658d70715320119c218d4
xchope <p.tran@student.htw-berlin.de>
2025-11-28 01:28:16 +0100

    test

--- src/main/java/htw/webtech/mywatchlist/watchlist/WatchlistController.java
+++ src/main/java/htw/webtech/mywatchlist/watchlist/WatchlistController.java
@@ -4,10 +4,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = {
-        "http://localhost:5173",
-        "https://watchlist-vuih.onrender.com"
-})
+@CrossOrigin
 @RestController
 @RequestMapping("/api/watchlist")
 public class WatchlistController {
@@ -25,12 +22,9 @@ public class WatchlistController {
 
     @PostMapping
     public WatchItem create(@RequestBody WatchItem item) {
-        // einfache Defaults, falls Frontend nur title schickt
         if (item.getType() == null) {
             item.setType("movie");
         }
-        // finished/rating bleiben Standardwerte (false / 0), wenn nicht gesetzt
-
         return repository.save(item);
     }
 }
